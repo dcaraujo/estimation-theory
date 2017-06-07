944935ee274e741731c98c0be69d6320eda7ff8a
araujo <araujo@gtel.ufc.br>
2017-06-07 16:53:23 -0300

    Draft of the system model (CAMSAP 2017).

---
+++ CAMSAP_2017/auto/bare_conf.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook
+ "bare_conf"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("IEEEtran" "conference")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "IEEEtran"
+    "IEEEtran10")
+   (LaTeX-add-labels
+    "eq:rx_signal"
+    "eq:frame"
+    "eq:output_combiner"
+    "eq:output_combiner_tensor"
+    "eq:tensor_model")
+   (LaTeX-add-bibliographies
+    "../../latex-aux/reference"))
+ :latex)
+
