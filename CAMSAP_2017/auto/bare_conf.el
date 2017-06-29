6e752df32ad9db1c99593a91063df48e264496dd
araujo <araujo@gtel.ufc.br>
2017-06-29 14:51:06 -0300

    First version submitted to EDAS system.

--- CAMSAP_2017/auto/bare_conf.el
+++ CAMSAP_2017/auto/bare_conf.el
@@ -14,10 +14,28 @@
    (LaTeX-add-labels
     "eq:rx_signal"
     "eq:frame"
+    "fig:frame"
     "eq:output_combiner"
     "eq:output_combiner_tensor"
-    "eq:tensor_model")
+    "eq:tensor_model"
+    "sec:multiway_cs"
+    "sec:parafac_model"
+    "eq:tap_model"
+    "eq:mode_product_parafac"
+    "eq:parafac_rx_signal"
+    "eq:sparse_parafac_rx_signal"
+    "th.:compressed_parafac"
+    "eq.uniqueness_compress_parafac"
+    "eq:l1_norm_modeOne"
+    "eq:sparse_mapping"
+    "sec:algorithm"
+    "alg.:als"
+    "eq:nmse"
+    "fig:tx_beams"
+    "fig:angleTx_error"
+    "fig:angleRx_error"
+    "fig:delay_error")
    (LaTeX-add-bibliographies
-    "../../latex-aux/reference"))
+    "latex-aux/reference"))
  :latex)
 
